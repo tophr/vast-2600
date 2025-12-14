9d28098108b905e3a63fc5d194c13efaab840bcb
Topher McCulloch <tophermcculloch@gmail.com>
2025-12-14 15:29:25 -0600

    Removes glitch specific files / updates src

--- src/scene/thief.js
+++ src/scene/thief.js
@@ -1,17 +1,17 @@
 export default function sceneThief() {
 	const traps = ["omen", "flamewall", "bomb", "monster"];
-  
-	for (const trap of traps) {
-	  loadSprite(trap, `${trap}.png`);
-	}
-  
+
+	// for (const trap of traps) {
+	//   loadSprite(trap, `${trap}.png`);
+	// }
+
 	scene("thiefScene", () => {
 	  // loadSound("hit", "/examples/sounds/hit.mp3");
 	  // loadSound("wooosh", "/examples/sounds/wooosh.mp3");
-  
+
 	  const SPEED_MIN = 40; //120;
 	  const SPEED_MAX = 160; //640;
-  
+
 	  // add the player game object
 	  const player = add([
 		sprite("thief"),
@@ -19,18 +19,18 @@ export default function sceneThief() {
 		area({ scale: 0.5 }),
 		anchor("center"),
 	  ]);
-  
+
 	  // make the layer move by mouse
 	  player.onUpdate(() => {
 		player.pos = mousePos();
 	  });
-  
+
 	  // game over if player eats a trap
 	  player.onCollide("trap", () => {
 		go("lose", score, "thief");
 		// play("hit");
 	  });
-  
+
 	  // move the obstacle every frame, destroy it if far outside of screen
 	  onUpdate("obstacle", (obstacle) => {
 		obstacle.move(-obstacle.speed, 0);
@@ -38,7 +38,7 @@ export default function sceneThief() {
 		  destroy(obstacle);
 		}
 	  });
-  
+
 	  onUpdate("coin", (coin) => {
 		if (coin.pos.x <= 0) {
 		  go("lose", score, "thief");
@@ -46,10 +46,10 @@ export default function sceneThief() {
 		  addKaboom(coin.pos);
 		}
 	  });
-  
+
 	  // score counter
 	  let score = 0;
-  
+
 	  const scoreLabel = add([
 		text(score, {
 		  font: "unscii",
@@ -57,7 +57,7 @@ export default function sceneThief() {
 		}),
 		pos(12, 12),
 	  ]);
-  
+
 	  // increment score if player eats a coin
 	  player.onCollide("coin", (coin) => {
 		addKaboom(player.pos, ({ scale: 0.25 }));
@@ -67,7 +67,7 @@ export default function sceneThief() {
 		burp();
 		shake(12);
 	  });
-  
+
 	  // do this every 0.3 seconds
 	  loop(0.3, () => {
 		// spawn from right side of the screen
@@ -79,7 +79,7 @@ export default function sceneThief() {
 		// 50% percent chance is coin
 		const isCoin = chance(0.5);
 		const spriteName = isCoin ? "coin" : choose(traps);
-  
+
 		add([
 		  sprite(spriteName),
 		  pos(x, y),
@@ -93,4 +93,3 @@ export default function sceneThief() {
 	  });
 	});
   }
-  
